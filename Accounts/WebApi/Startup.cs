f67399162abeaffa80285fd49b0f91c538ac355b
Georgiana-hub <botez.georgiana99@gmail.com>
2021-05-26 10:53:50 +0300

    update

--- Accounts/WebApi/Startup.cs
+++ Accounts/WebApi/Startup.cs
@@ -32,7 +32,7 @@ namespace WebApi
 
             services.AddCors((options) =>
             {
-                options.AddPolicy("MyPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+                options.AddPolicy("MyPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials());
             });
 
             services.AddMvc(option => option.EnableEndpointRouting = false);
